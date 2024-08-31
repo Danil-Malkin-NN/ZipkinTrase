f43590b7adc0b12c4071673772cace4cdad09859
Danil <danilmalkin2015@gmail.com>
2024-08-31 17:50:00 +0300

    Настроил трасировку из сервиса в сервис и добавил красивый запуск из docker-compose

---
+++ service/src/main/java/ru/danil/zipkintest/controllers/SimpleController.java
@@ -0,0 +1,34 @@
+package ru.danil.zipkintest.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.danil.zipkintest.feign.SimpleApi;
+
+import java.util.Optional;
+
+@Slf4j
+@RestController
+public class SimpleController {
+
+    private final Optional<SimpleApi> simpleApi;
+    private final String name;
+
+    public SimpleController(@Autowired(required = false) SimpleApi simpleApi,
+                            @Value("${spring.application.name}") String name) {
+        if (simpleApi == null) {
+            log.warn("Фейн для внешнего клиента не найден");
+        }
+        this.simpleApi = Optional.ofNullable(simpleApi);
+        this.name = name;
+    }
+
+
+    @GetMapping("")
+    public String getName() {
+        return name + " " + simpleApi.map(SimpleApi::getName)
+                .orElse("end");
+    }
+}
--- service/src/main/java/ru/danil/zipkintest/feign/SimpleApi.java
+++ service/src/main/java/ru/danil/zipkintest/feign/SimpleApi.java
@@ -1,8 +1,10 @@
 package ru.danil.zipkintest.feign;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@ConditionalOnProperty(name = "feign.url")
 @FeignClient(value = "zipkinValue", url = "${feign.url}")
 public interface SimpleApi {
 
