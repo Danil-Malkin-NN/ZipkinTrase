2c7f87bfd40035a0c3911551bd526c46e6283e3b
Danil <danilmalkin2015@gmail.com>
2024-08-31 20:19:53 +0300

    Заработал пример с кешем.

--- service/src/main/java/ru/danil/zipkintest/feign/SimpleApi.java
+++ service/src/main/java/ru/danil/zipkintest/feign/SimpleApi.java
@@ -1,13 +1,15 @@
 package ru.danil.zipkintest.feign;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @ConditionalOnProperty(name = "feign.url")
-@FeignClient(value = "zipkinValue", url = "${feign.url}")
+@FeignClient(value = "zipkinValueCash", url = "${feign.url}")
 public interface SimpleApi {
 
+    @Cacheable(value = "name")
     @GetMapping
     String getName();
 
