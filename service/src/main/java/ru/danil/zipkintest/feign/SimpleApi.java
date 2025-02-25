3c424b56a66aef28a130d1c0cb47364f1fdd54e2
Danil <danilmalkin2015@gmail.com>
2025-02-25 09:50:18 +0300

    Добавил name в путь

--- service/src/main/java/ru/danil/zipkintest/feign/SimpleApi.java
+++ service/src/main/java/ru/danil/zipkintest/feign/SimpleApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface SimpleApi {
 
     @Cacheable(value = "name")
-    @GetMapping
+    @GetMapping("name")
     String getName();
 
 }
